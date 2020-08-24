6910febfe5efd8f7fd8838e72f3083a9bd694bb9
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-08-25 01:30:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import  Flask
+import requests
+from bs4 import BeautifulSoup
+import html5lib
+app=Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world"
+@app.route("/repo/<id>")
+def hh(id):
+    urll="https://github.com/"+id+"?tab=repositories"
+    r=requests.get(urll)
+    soup=BeautifulSoup(r.content,'html5lib')
+    a=soup.find(id="user-repositories-list")
+    z=a.find_all('li')
+    yo={}
+    er={}
+    
+    for i in range(0,len(z)):
+        s=z[i]
+        
+        try:
+            y=s.find('a')
+            x=y['href']
+            try:
+                ur=[]
+                sds=s.find('a',class_="muted-link mr-3").getText()
+                
+
+                j=s.find('p').getText()
+                name=y.getText()
+                url="https://github.com"+x
+                k={'name':name.strip(),'description':j.strip(),'url':url,"stars":sds.strip()}
+            except:
+                ur=[]
+                h=y.getText()
+                y="https://github.com"+x
+                k={'name': h.strip(),'url':y,"stars":sds.strip()}
+            rep="rep"+str(i)
+            yo[rep]=k
+            
+        except:
+            print("none")
+        
+    return(yo)
+        
+    
+if __name__ == "__main__":
+    app.run()
