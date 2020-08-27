2831d4561cc95aefb368d6d513a5412a15d0fe14
Fredy Somy <65971072+fredysomy@users.noreply.github.com>
2020-08-27 23:22:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import  Flask
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask import jsonify
 from flask_cors import CORS
 import requests
@@ -8,7 +8,7 @@ app=Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route("/")
 def hello():
-    return "hello world"
+    return render_template('docs.html')
 @app.route("/repo/<id>")
 def hh(id):
     urll="https://github.com/"+id+"?tab=repositories"
@@ -51,17 +51,26 @@ def st(id):
     urll="https://github.com/"+id
     r=requests.get(urll)
     soup=BeautifulSoup(r.content,'html5lib')
+    sdd=soup.find("h1" ,class_="vcard-names pl-2 pl-md-0")
+    name=sdd.find_all('span')[0].getText()
+    u_name=sdd.find_all('span')[1].getText()
     asd=soup.find(class_="flex-order-1 flex-md-order-none mt-2 mt-md-0")
     dd=asd.find(class_="mb-3")
     followers=dd.find_all('a')[0].find('span').getText().strip(' ')
     following=dd.find_all('a')[1].find('span').getText().strip(' ')
     totstars=dd.find_all('a')[2].find('span').getText().strip(' ')
+    asd=soup.find(class_="flex-order-1 flex-md-order-none mt-2 mt-md-0")
+    
+    
+    dsdf=soup.find(class_="avatar avatar-user width-full border bg-white")['src']
+    repo_num=soup.find(class_="UnderlineNav-body").find('span',class_="Counter").getText()
+    rr=""
 
-
-    las={'Followers':followers,'Following':following,'Total_stars':totstars}
+    las={'Name':name,'Username':u_name,"ProfilePic":dsdf,'Followers':followers,'Following':following,'Total_stars':totstars,"Total-repos":repo_num}
     return(las)
    
 
     
 if __name__ == "__main__":
     app.run()
+
